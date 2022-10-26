a793b333311984a5ddfbf7c4bd6690b5c964c66f
Guilherme Alencar <programador.alencar@hotmail.com>
2022-10-26 19:08:04 -0300

    commit

--- perceptron/Program.cs
+++ perceptron/Program.cs
@@ -1,25 +1,25 @@
 ﻿
-int[] entrada = new[] { 0, 1, 2 };
+int[] entradas = new[] { 0, 1, 2 };
+Console.WriteLine($"Entradas: {entradas[0]}, {entradas[1]}, {entradas[2]}");
 
 int[] pesos = new[] { 1, 1, 1 };
+Console.WriteLine($"Pesos: {pesos[0]}, {pesos[1]}, {pesos[2]}");
 
 int[] abias = new[] { 1, 1, 1 };
+Console.WriteLine($"Abias: {abias[0]}, {abias[1]}, {abias[2]}");
 
 int[] neuronios = new[] {
-    pesos[0] * entrada[0] + abias[0],
-    pesos[1] * entrada[1] + abias[1],
-    pesos[2] * entrada[2] + abias[2]
+    pesos[0] * entradas[0] + abias[0],
+    pesos[1] * entradas[1] + abias[1],
+    pesos[2] * entradas[2] + abias[2]
 };
+
+Console.WriteLine($"Neurônios: {neuronios[0]}, {neuronios[1]}, {neuronios[2]}");
 
 int[] saidas = new[] {
-    1,
-    2,
-    3
+    neuronios[0] > 0 ? 1 : 0,
+    neuronios[1] > 0 ? 1 : 0,
+    neuronios[2] > 0 ? 1 : 0
 };
 
-int[] ativacao = new[] {
-    1,
-    2,
-    3
-};
-
+Console.WriteLine($"Saídas: {saidas[0]}, {saidas[1]}, {saidas[2]}");
